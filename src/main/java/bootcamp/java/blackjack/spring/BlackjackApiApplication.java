86c4aa95684616fc12431b074f3a765c10074c6a
Josh Stothfang <josh@joshstothfang.com>
2023-09-25 09:17:17 -0400

    Initial Commit

---
+++ src/main/java/bootcamp/java/blackjack/spring/BlackjackApiApplication.java
@@ -0,0 +1,13 @@
+package bootcamp.java.blackjack.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackjackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackjackApiApplication.class, args);
+	}
+
+}
